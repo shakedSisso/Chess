a1a855e006e1faaca14d914265e22a48d23c98c1
Ofer Ovadia <ofer026@gmail.com>
2022-12-29 17:11:59 +0200

    Added commenting to InvalidMoveException class (.h functions and cpp code).

--- Chess/InvalidMoveException.h
+++ Chess/InvalidMoveException.h
@@ -5,8 +5,35 @@
 class InvalidMoveException : public std::exception
 {
 public:
+	/*
+	* Function: InvalidMoveException
+	* ----------------------------
+	*   The function builds a new object of the class InvalidMoveException with the exception's type
+	*
+	*   const int type: the type of the exception
+	*
+	*   returns: builder.
+	*/
 	InvalidMoveException(const int type);
+	/*
+	* Function: what
+	* ----------------------------
+	*   The function returns a string with explantions of what happend and why the exception was thrown
+	*
+	*   input: none
+	*
+	*   returns: std::string. An explanation of what happend
+	*/
 	std::string what();
+	/*
+	* Function: getType const
+	* ----------------------------
+	*   The function returns the current type of the exception
+	*
+	*   input: none
+	*
+	*   returns: int. The type of the exception
+	*/
 	int getType() const;
 	enum types { NOT_PLAYER_PIECE = 2, SELF_EATING, SELF_CHESS, ILLEGAL_INDEX, ILLEGAL_MOVE, SAME_PLACE };
 	static types type;
