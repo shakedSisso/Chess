619a874626518f426133b30a733d83da6be9822f
Shaked Sisso <sissoshaked@gmail.com>
2023-01-02 22:16:27 +0200

    Implemented isLegalMove and setDidMoveToTrue in Pawn class.

--- Chess/Pawn.cpp
+++ Chess/Pawn.cpp
@@ -1,7 +1,8 @@
 #include "Pawn.h"
+#include "InvalidMoveException.h"
 
 Pawn::Pawn(const int row, const int col, const bool isWhite)
-	: Piece(row, col, isWhite)
+	: Piece(row, col, isWhite), _didMove(false)
 {
 }
 
@@ -11,7 +12,76 @@ Pawn::~Pawn()
 
 bool Pawn::isLegalMove(const int row, const int col, const Board& board) const
 {
-	return false;
+	if (this->_row == row && this->_col == col) // checking if we are trying to move to the same place
+	{
+		throw InvalidMoveException(InvalidMoveException::types::SAME_PLACE);
+	}
+	if (board.getPiece(row, col) != nullptr && board.getPiece(row, col)->getIsWhite() == this->_isWhite) // checking that we are not trying to move to a place that have a piece with the same color
+	{
+		throw InvalidMoveException(InvalidMoveException::types::SELF_EATING);
+	}
+	if (this->_isWhite)
+	{
+		if (this->_row + 2 < row)
+		{
+			throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+		}
+		if (this->_col != col && this->_row + 1 == row)
+		{
+			if (this->_col - 1 == col || this->_col + 1 == col)
+			{
+				if (board.getPiece(row, col) == nullptr)
+				{
+					throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+				}
+			}
+			else
+			{
+				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+			}
+		}
+		else
+		{
+			if (this->_didMove && this->_row + 2 == row)
+			{
+				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+			}
+		}
+	}
+	else
+	{
+		if (this->_row - 2 > row)
+		{
+			throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+		}
+		if (this->_col != col && this->_row - 1 == row)
+		{
+			if (this->_col - 1 == col || this->_col + 1 == col)
+			{
+				if (board.getPiece(row, col) == nullptr)
+				{
+					throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+				}
+			}
+			else
+			{
+				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+			}
+		}
+		else
+		{
+			if (this->_didMove && this->_row - 2 == row)
+			{
+				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+			}
+		}
+	}
+	return true;
+}
+
+void Pawn::setDidMoveToTrue()
+{
+	this->_didMove = true;
 }
 
 std::string Pawn::toString() const
