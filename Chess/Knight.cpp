2e940687afb991a6a8dd35c0ef7105fa5478cbcd
Ofer Ovadia <ofer026@gmail.com>
2023-01-02 20:38:02 +0200

    Started implementing isLegalMove on Knight class. Not finished

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -1,4 +1,8 @@
 #include "Knight.h"
+#include "InvalidMoveException.h"
+
+#define POSSIBLE_POSITIONS 8
+//#define POSSIBLE_COLS 4
 
 Knight::Knight(const int row, const int col, const bool isWhite)
     : Piece(row, col, isWhite)
@@ -11,7 +15,28 @@ Knight::~Knight()
 
 bool Knight::isLegalMove(const int row, const int col, const Board& board) const
 {
-    return false;
+	//int possiblePlacesRows[POSSIBLE_ROWS] = { this->_row + 1, this->_row - 1, this->_row  + 2, this->_row - 1 };
+	//int possiblePlacesCols[POSSIBLE_COLS] = { this->_col + 1, this->_col - 1, this->_col + 2, this->_col - 2 };
+	int possiblePlacesRows[POSSIBLE_POSITIONS] = {this->_row - 1, this->_row - 2, this->_row - 2, this->_row - 1, this->_row + 1, this->_row + 2, this->_row + 2, this->_row + 1};
+	int possiblePlacesCols[POSSIBLE_POSITIONS] = {this->_col - 2, this->_col - 1, this->_col + 1, this->_col + 2, this->_col + 2, this->_col + 1, this->_col - 1, this->_col - 2};
+	bool isValid = false;
+	int i = 0;
+	if (this->_row == row && this->_col == col) // checking if we are trying to move to the same place
+	{
+		throw InvalidMoveException(InvalidMoveException::types::SAME_PLACE);
+	}
+	for (i = 0; i < POSSIBLE_POSITIONS && !isValid; i++)
+	{
+		if (row == possiblePlacesRows[i] && col == possiblePlacesCols[i])
+		{
+			isValid = true;
+		}
+	}
+	if (!isValid)
+	{
+		throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
+	}
+	
 }
 
 std::string Knight::toString() const
