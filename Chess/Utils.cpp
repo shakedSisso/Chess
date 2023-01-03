3bd1e420aeed906eda876846845d480b2c3ef8da
Ofer Ovadia <ofer026@gmail.com>
2023-01-03 21:48:35 +0200

    Fixed utils changeStringToPlaces function to translate string correctly

--- Chess/Utils.cpp
+++ Chess/Utils.cpp
@@ -1,4 +1,5 @@
 #include "Utils.h"
+#include <math.h>
 
 #define SMALL_A_ASCII_VALUE 97
 #define ZERO_ASCII_VALUE 48
@@ -6,10 +7,11 @@
 
 int* Utils::changeStringToPlaces(std::string place)
 {
-	int* places = new int[ITEM_AMOUNT];
-	places[0] = int(place[0]) - SMALL_A_ASCII_VALUE;
-	places[1] = int(place[1]) - ZERO_ASCII_VALUE;
-	places[2] = int(place[2]) - SMALL_A_ASCII_VALUE;
-	places[3] = int(place[3]) - ZERO_ASCII_VALUE;
+	int* places = new int[ITEM_AMOUNT]; 
+	places[1] = int(place[0]) - SMALL_A_ASCII_VALUE;
+	places[0] = abs(int(place[1]) - ZERO_ASCII_VALUE - 8);
+	places[3] = int(place[2]) - SMALL_A_ASCII_VALUE;
+	places[2] = abs(int(place[3]) - ZERO_ASCII_VALUE - 8);
+
 	return places;
 }
