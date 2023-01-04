e30864af105b1fe5eaf94f755879559786bf10da
Ofer Ovadia <ofer026@gmail.com>
2023-01-04 08:47:45 +0200

    Added commenting to Utils class and change the param on changeStringToPlaces to const

--- Chess/Utils.cpp
+++ Chess/Utils.cpp
@@ -4,14 +4,15 @@
 #define SMALL_A_ASCII_VALUE 97
 #define ZERO_ASCII_VALUE 48
 #define ITEM_AMOUNT 4	
+#define MAX_ROW 8
 
-int* Utils::changeStringToPlaces(std::string place)
+int* Utils::changeStringToPlaces(const std::string place)
 {
 	int* places = new int[ITEM_AMOUNT]; 
 	places[1] = int(place[0]) - SMALL_A_ASCII_VALUE;
-	places[0] = abs(int(place[1]) - ZERO_ASCII_VALUE - 8);
+	places[0] = abs(int(place[1]) - ZERO_ASCII_VALUE - MAX_ROW); // reverting the value as in the graphics the top row is 8 and in our board the top row is 0
 	places[3] = int(place[2]) - SMALL_A_ASCII_VALUE;
-	places[2] = abs(int(place[3]) - ZERO_ASCII_VALUE - 8);
+	places[2] = abs(int(place[3]) - ZERO_ASCII_VALUE - MAX_ROW); // reverting the value as in the graphics the top row is 8 and in our board the top row is 0
 
 	return places;
 }
--- Chess/Utils.h
+++ Chess/Utils.h
@@ -4,5 +4,15 @@
 class Utils
 {
 public:
-	static int* changeStringToPlaces(std::string place);
+	/*
+	* Function: static changeStringToPlaces
+	* ----------------------------
+	*   The function gets a string with two places on the board. The function translates the places on the board (represented by a letter for the col and a number for the row) to two numbers, the row first than the col.
+	*	The function returns a pointer to an array with 4 cells, the first two cells are the row and col of the first place on the string and the last two cells are the row and col of the second place on the string
+	*
+	*   const std::string place: the string of the places to change to numerical rows and cols of the board
+	*
+	*   returns: int*. A pointer to an array with the converted places from the string given
+	*/
+	static int* changeStringToPlaces(const std::string place);
 };
