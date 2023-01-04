7ca72b456bbd08fab038013347de5d92a40f9b97
Ofer Ovadia <ofer026@gmail.com>
2023-01-04 08:35:18 +0200

    Added commenting to Pawn class

--- Chess/Pawn.cpp
+++ Chess/Pawn.cpp
@@ -20,17 +20,17 @@ bool Pawn::isLegalMove(const int row, const int col, const Board& board) const
 	{
 		throw InvalidMoveException(InvalidMoveException::types::SELF_EATING);
 	}
-	if (this->_isWhite)
+	if (this->_isWhite) // checking if we are white in order to check direction of movement
 	{
-		if (this->_row + 2 < row)
+		if (this->_row + 2 < row) // checking if the place we are trying to go to is far from the current place by at least 3 rows (that means the move is not legal by the rules, a pawn can't move up or down more than two rows)
 		{
 			throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 		}
-		if (this->_col != col && this->_row + 1 == row)
+		if (this->_col != col && this->_row + 1 == row) // checking if we are eating a piece and moving diagonaly
 		{
-			if (this->_col - 1 == col || this->_col + 1 == col)
+			if (this->_col - 1 == col || this->_col + 1 == col) // checking that we are not trying to move more than one place left or right (not legal by the rules)
 			{
-				if (board.getPiece(row, col) == nullptr)
+				if (board.getPiece(row, col) == nullptr) // checking that there is a piece on the place we are moving to diagonaly (by the rules, can only move diagonaly if there is a piece to eat)
 				{
 					throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 				}
@@ -42,11 +42,11 @@ bool Pawn::isLegalMove(const int row, const int col, const Board& board) const
 		}
 		else
 		{
-			if (this->_didMove && this->_row + 2 == row)
+			if (this->_didMove && this->_row + 2 == row) // checking if we've already did the first move and if so checking if we are trying to move up or down by two places (can only move by 2 on the first move by the rules)
 			{
 				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 			}
-			if (board.getPiece(row, col) != nullptr)
+			if (board.getPiece(row, col) != nullptr) // checking if there is a piece in front of it since it can only straight if there is not piece on the way
 			{
 				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 			}
@@ -54,15 +54,15 @@ bool Pawn::isLegalMove(const int row, const int col, const Board& board) const
 	}
 	else
 	{
-		if (this->_row - 2 > row)
+		if (this->_row - 2 > row) // checking if the place we are trying to go to is far from the current place by at least 3 rows (that means the move is not legal by the rules, a pawn can't move up or down more than two rows)
 		{
 			throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 		}
-		if (this->_col != col && this->_row - 1 == row)
+		if (this->_col != col && this->_row - 1 == row) // checking if we are eating a piece and moving diagonaly
 		{
-			if (this->_col - 1 == col || this->_col + 1 == col)
+			if (this->_col - 1 == col || this->_col + 1 == col) // checking that we are not trying to move more than one place left or right (not legal by the rules)
 			{
-				if (board.getPiece(row, col) == nullptr)
+				if (board.getPiece(row, col) == nullptr) // checking that there is a piece on the place we are moving to diagonaly (by the rules, can only move diagonaly if there is a piece to eat)
 				{
 					throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 				}
@@ -74,11 +74,11 @@ bool Pawn::isLegalMove(const int row, const int col, const Board& board) const
 		}
 		else
 		{
-			if (this->_didMove && this->_row - 2 == row)
+			if (this->_didMove && this->_row - 2 == row) // checking if we've already did the first move and if so checking if we are trying to move up or down by two places (can only move by 2 on the first move by the rules)
 			{
 				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 			}
-			if (board.getPiece(row, col) != nullptr)
+			if (board.getPiece(row, col) != nullptr) // checking if there is a piece in front of it since it can only straight if there is not piece on the way
 			{
 				throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 			}
--- Chess/Pawn.h
+++ Chess/Pawn.h
@@ -28,6 +28,18 @@ public:
     */
     ~Pawn();
 
+    /*
+    * Function: isLegalMove
+    * ----------------------------
+    *   The function gets a row and col on the board to check the move to and the board to check for other pieces. The function checks if the pawn can move to that location, and returns true if it cans.
+    *   Otherwise, if the move is not valid it will throw an InvalidMoveException with according type of reason why the move is not legal
+    *
+    *   const int row: the row to move the pawn to
+    *   const int col: the col to move the pawn to
+    *   const Board& board: the reference to the playing board to check for other piece on the board that can invalidate the move
+    *
+    *   returns: bool. true if the move is valid and will throw an exception (InvalidMoveException) with the type if the move is not valid
+    */
     bool isLegalMove(const int row, const int col, const Board& board) const override;
 
     /*
@@ -40,6 +52,15 @@ public:
     *   returns: std::string. The textual representation of the piece
     */
     std::string toString() const override;
+    /*
+    * Function: setDidMoveToTrue
+    * ----------------------------
+    *   The function sets the didMove field to true
+    *
+    *   inout: none
+    *
+    *   returns: void.
+    */
     void setDidMoveToTrue();
 private:
     bool _didMove;
