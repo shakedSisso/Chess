2987825c47130ae38ab156042ed4e7b18bff8068
Ofer Ovadia <ofer026@gmail.com>
2023-01-04 08:25:00 +0200

    Added commenting to Knight class

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -14,6 +14,7 @@ Knight::~Knight()
 
 bool Knight::isLegalMove(const int row, const int col, const Board& board) const
 {
+	// creating two arrays that with every index from both arrays we have a place that the knight can move to by the rules of the game
 	int possiblePlacesRows[POSSIBLE_POSITIONS] = {this->_row - 1, this->_row - 2, this->_row - 2, this->_row - 1, this->_row + 1, this->_row + 2, this->_row + 2, this->_row + 1};
 	int possiblePlacesCols[POSSIBLE_POSITIONS] = {this->_col - 2, this->_col - 1, this->_col + 1, this->_col + 2, this->_col + 2, this->_col + 1, this->_col - 1, this->_col - 2};
 	bool isValid = false;
@@ -22,14 +23,14 @@ bool Knight::isLegalMove(const int row, const int col, const Board& board) const
 	{
 		throw InvalidMoveException(InvalidMoveException::types::SAME_PLACE);
 	}
-	for (i = 0; i < POSSIBLE_POSITIONS && !isValid; i++)
+	for (i = 0; i < POSSIBLE_POSITIONS && !isValid; i++) // going through the possible allowed places and checking if the we have been given is among those allowed places
 	{
 		if (row == possiblePlacesRows[i] && col == possiblePlacesCols[i])
 		{
 			isValid = true;
 		}
 	}
-	if (!isValid)
+	if (!isValid) // if the flag wasn't changed to true that means that the place we have been given is not a place the knight can move to by the rules of the game
 	{
 		throw InvalidMoveException(InvalidMoveException::types::ILLEGAL_MOVE);
 	}
