21922d005835df3d068be8e0b50fc3b7c45c1407
Ofer Ovadia <ofer026@gmail.com>
2023-01-04 09:01:45 +0200

    Added commenting to Player class

--- Chess/Player.h
+++ Chess/Player.h
@@ -3,8 +3,35 @@
 class Player
 {
 public:
+	/*
+	* Function: Player
+	* ----------------------------
+	*   The function builds a new object of the class Player with the player's color (black or white using a bool)
+	*
+	*   const bool isWhite: the color of the player. true for white, false for black
+	*
+	*   returns: builder.
+	*/
 	Player(bool isWhite);
+	/*
+	* Function: ~Player
+	* ----------------------------
+	*   The function is the destructor for the object
+	*
+	*   input: none
+	*
+	*   returns: destructor.
+	*/
 	~Player();
+	/*
+	* Function: getIsWhite const
+	* ----------------------------
+	*   The function returns the color of the player
+	*
+	*   input: none
+	*
+	*   returns: bool. True if the player is white, false if the player is black
+	*/
 	bool getIsWhite() const;
 private:
 	bool _isWhite;
